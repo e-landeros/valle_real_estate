1e99d61f6986453a96629958fa6ac68c7b728487
e-landeros <landerosedgard@gmail.com>
2019-01-19 20:10:19 -0800

    changed static routes

--- valle_real_estate/settings.py
+++ valle_real_estate/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIR = [
-    os.path.join(BASE_DIR, 'btre/static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'valle_real_estate/static')
 ]
+
