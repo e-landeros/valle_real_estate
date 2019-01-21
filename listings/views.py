cd271d9f1749fd9b20b27f8190ced3232a6b0e5e
e-landeros <landerosedgard@gmail.com>
2019-01-21 11:38:31 -0800

    created search context for db filtering and dynamic display on frontend

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from .models import Listing
+from .choices import price_choices, bedroom_choices, state_choices
 
 def index(request):
     #get listings from db
@@ -27,4 +28,43 @@ def listing(request, listing_id):
     return render(request, 'listings/listing.html', context)
 
 def search(request):
-    return render(request, 'listings/search.html')
+    # build query set list for search
+    queryset_list = Listing.objects.order_by('-list_date')
+
+    #keywords search : Get data from form field. name="keywords"
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_list = queryset_list.filter(description__icontains=keywords) # icontains search text
+    
+    # City search 
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            queryset_list = queryset_list.filter(city__iexact=city) #iexact is case insensitive. use exact for case sensitive search
+
+    # State search 
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            queryset_list = queryset_list.filter(state__iexact=state)
+
+    # Bedrooms search 
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            queryset_list = queryset_list.filter(bedrooms__lte=bedrooms) #lte will return bedroom #'s less than or equalto. can use exact for differetn filter
+
+    # Price search 
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            queryset_list = queryset_list.filter(price__lte=price)
+
+    context = {
+        'state_choices':state_choices,
+        'bedroom_choices':bedroom_choices,
+        'price_choices':price_choices,
+        'listings': queryset_list
+    }
+    return render(request, 'listings/search.html', context)
