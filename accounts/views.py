bab42dd3ca15d769d23205aad1b8e6a320d3dc22
e-landeros <landerosedgard@gmail.com>
2019-01-21 13:06:45 -0800

    accoutns markup. need views for form logic

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render, redirect
 
 def register(request):
-    return render(request, 'accounts/register.html')
+    if request.method == 'POST': #form submition
+        #register user
+        pass
+    else: return render(request, 'accounts/register.html')
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST': #form submition
+        #login user
+        pass
+    else: return render(request, 'accounts/login.html')
 
 def logout(request):
     return redirect('index')
